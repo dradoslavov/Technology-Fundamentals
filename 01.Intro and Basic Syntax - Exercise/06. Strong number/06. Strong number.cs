cf0b5dbd61a31d3ba8da8d56e386069f7009ba34
dradoslavov <46215142+dradoslavov@users.noreply.github.com>
2019-01-02 17:04:45 +0200

    Add files via upload

---
+++ 01.Intro and Basic Syntax - Exercise/06. Strong number/06. Strong number.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            long inputNum = long.Parse(Console.ReadLine());
+            long factorialSum = 0;
+            long checkNum = inputNum;
+
+            while (checkNum != 0)
+            {
+
+                long result = checkNum % 10;
+                checkNum /= 10;
+
+                long factorial = 1;
+                for (int i = 1; i <= result; i++)
+                {
+                    factorial *= i;
+                }
+                factorialSum += factorial;
+            }
+
+            if (factorialSum == inputNum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ 01.Intro and Basic Syntax - Exercise/07. Vending Machine/07. Vending Machine.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace _07.Vending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            decimal money = 0;
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "Start")
+                {
+                    break;
+                }
+                decimal coin = decimal.Parse(command);
+                if (coin == 0.1m || coin == 0.2m || coin == 0.5m || coin == 1m || coin == 2m)
+                {
+                    money += coin;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coin}");
+                }
+
+
+            }
+            while (true)
+            {
+
+
+                string drink = Console.ReadLine().ToLower();
+                if (drink == "end")
+                {
+                    Console.WriteLine($"Change: {money:F2}");
+
+                    break;
+                }    
+
+                if (drink == "nuts")
+                {
+                    if (money < 2)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        
+                    }
+                    else
+                    {
+                        money -= 2;
+
+                        Console.WriteLine("Purchased nuts");
+                        
+
+                    }
+                }
+                else if (drink == "water")
+                {
+                    if (money < 0.7m)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        
+
+                    }
+                    else
+                    {
+                        money -= 0.7m;
+
+                        Console.WriteLine("Purchased water");
+                        
+                    }
+
+
+                }
+                else if (drink == "crisps")
+                {
+                    if (money < 1.5m)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        
+
+                    }
+                    else
+                    {
+                        money -= 1.5m;
+
+                        Console.WriteLine("Purchased crisps");
+                        
+                    }
+
+                }
+                else if (drink == "soda")
+                {
+                    if (money < 0.8m)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        
+
+                    }
+                    else
+                    {
+                        money -= 0.8m;
+
+                        Console.WriteLine("Purchased soda");
+                        
+
+                    }
+
+                }
+                else if (drink == "coke")
+                {
+                    if (money < 1)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                       
+
+                    }
+                    else
+                    {
+                        money -= 1.0m;
+
+                        Console.WriteLine("Purchased coke");
+                        
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product");
+
+                }
+
+
+            }
+
+        }
+
+    }
+}

da8bd05b2242db0ff150bfa3d1133b6cb135a32c
dradoslavov <46215142+dradoslavov@users.noreply.github.com>
2019-01-11 20:27:55 +0200

    Add files via upload

---
+++ 02. Change List/02. Change List.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string firstCommand = null;
+            while (firstCommand!="end")
+            {
+                List<string> command = Console.ReadLine().Split().ToList();
+                firstCommand = command[0];
+                if (firstCommand=="Delete")
+                {
+                    int element = int.Parse(command[1]);
+
+                    list.Remove(element);
+                }
+                else if (firstCommand=="Insert")
+                {
+                    int element = int.Parse(command[1]);
+                    int position = int.Parse(command[2]);
+                    list.Insert(position, element);
+                }
+            }
+            PrintList(list);
+        }
+
+        private static void PrintList(List<int> list)
+        {
+            Console.WriteLine($"{string.Join(" ", list)}");
+        }
+    }
+}

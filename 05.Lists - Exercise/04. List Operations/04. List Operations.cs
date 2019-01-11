5f264b726e5d783a18fa48f3d931b131773c1596
dradoslavov <46215142+dradoslavov@users.noreply.github.com>
2019-01-11 20:40:56 +0200

    Add files via upload

---
+++ 05.Lists - Exercise/04. List Operations/04. List Operations.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._List_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<string> commands = Console.ReadLine().Split().ToList();
+
+            while (commands[0] != "End")
+            {
+                if (commands[0] == "Add")
+                {
+                    list.Add(int.Parse(commands[1]));
+                }
+
+                else if (commands[0] == "Insert")
+                {
+                    if (int.Parse(commands[2]) >= 0 && int.Parse(commands[2]) < list.Count)
+                    {
+                        list.Insert(int.Parse(commands[2]), int.Parse(commands[1]));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+
+                }
+
+                else if (commands[0] == "Remove")
+                {
+                    if (int.Parse(commands[1]) >= 0 && int.Parse(commands[1]) < list.Count)
+                    {
+                        list.RemoveAt(int.Parse(commands[1]));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+
+                }
+                else if (commands[0] == "Shift")
+                {
+                    if (commands[1] == "left")
+                    {
+                        list = ShiftLeft(list, int.Parse(commands[2]));
+                    }
+                    else if (commands[1] == "right")
+                    {
+                        list = ShiftRight(list, int.Parse(commands[2]));
+                    }
+                }
+
+                commands = Console.ReadLine().Split().ToList();
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+        }
+
+        private static List<int> ShiftRight(List<int> listOfInt, int steps)
+        {
+            var shiftedList = new List<int>();
+
+            for (int i = listOfInt.Count - (steps % listOfInt.Count); i < listOfInt.Count; i++)
+            {
+                shiftedList.Add(listOfInt[i]);
+            }
+
+            for (int i = 0; i < listOfInt.Count - (steps % listOfInt.Count); i++)
+            {
+                shiftedList.Add(listOfInt[i]);
+            }
+
+            return shiftedList;
+        }
+
+        private static List<int> ShiftLeft(List<int> listOfInt, int steps)
+        {
+            var shiftedList = new List<int>();
+
+            for (int i = steps % listOfInt.Count; i < listOfInt.Count; i++)
+            {
+                shiftedList.Add(listOfInt[i]);
+            }
+
+            for (int i = 0; i < steps % listOfInt.Count; i++)
+            {
+                shiftedList.Add(listOfInt[i]);
+            }
+
+            return shiftedList;
+        }
+    }
+}
---
+++ 05.Lists - Exercise/05. Bomb Numbers/05. Bomb Numbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> bomb = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int bombN = bomb[0];
+            int range = bomb[1];
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+                if (currentNumber==bombN)
+                {
+                    int startNumber = i - range;
+                    int finishNumber = i + range;
+                    if (startNumber<0)
+                    {
+                        startNumber = 0;
+                    }
+                    if (finishNumber>numbers.Count-1)
+                    {
+                        finishNumber = numbers.Count-1;
+                    }
+                    numbers.RemoveRange(startNumber, finishNumber-startNumber+1);
+                    i = 0;
+                }
+            }
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}

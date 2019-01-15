3f0bd7cd0aff3341a52447b0d82e05efc5215cb8
dradoslavov <46215142+dradoslavov@users.noreply.github.com>
2019-01-15 14:10:49 +0200

    Add files via upload

---
+++ 07. Objects-and-Classes-Lab/03. Big Factorial/03. Big Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _03._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+
+            for (int i = 1; i <= factorial; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

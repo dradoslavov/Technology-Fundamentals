95769692ee0d3e8499da0fb16f65ccbb64dd2fbf
dradoslavov <46215142+dradoslavov@users.noreply.github.com>
2019-01-15 15:29:36 +0200

    Add files via upload

---
+++ 08. Strings-and-Text-Processing-Lab/04. Text Filter/04. Text Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bannWord = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < bannWord.Length; i++)
+            {
+                string currentWord = new string('*', bannWord[i].Length);
+
+                text = text.Replace(bannWord[i], currentWord);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
